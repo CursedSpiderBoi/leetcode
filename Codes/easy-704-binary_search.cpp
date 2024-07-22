21c1fd571186ea8fb8e41793bf1b5fb91edbd7c1
Matthew Goembel <mgoembel1@gmail.com>
2024-07-22 19:51:50 -0400

    Update easy-704-binary_search.cpp

--- Codes/easy-704-binary_search.cpp
+++ Codes/easy-704-binary_search.cpp
@@ -1,4 +1,14 @@
 class Solution {
+
+/**
+* Given an array of integers, which is sorted in ascending order, and an integer target, 
+* The function searches the target in the array. If the target exists, return its index. Otherwise, return -1.
+* The algorithm operates in O(log n) runtime complexity.
+*
+* @param v Array of ascending order integers
+* @param k The target integer
+*/
+
 public:
     int search(vector<int>& v, int k) {
         int i =0,j=v.size()-1,mid;
