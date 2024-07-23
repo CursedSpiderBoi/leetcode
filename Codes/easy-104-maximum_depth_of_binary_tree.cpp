6992aabb464bf2c0faf320aaad96f61bdde27da3
Matthew Goembel <mgoembel1@gmail.com>
2024-07-22 21:04:11 -0400

    Update and rename BinaryTree_MaxDepthr.cpp to easy-104-maximum_depth_of_binary_tree.cpp

--- Codes/easy-104-maximum_depth_of_binary_tree.cpp
+++ Codes/easy-104-maximum_depth_of_binary_tree.cpp
@@ -1,3 +1,23 @@
+// You are given the root of a binary tree.
+
+// Return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+// Example 1:
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+
+// Example 2:
+
+// Input: root = [1,null,2]
+// Output: 2
+    
+
+    
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,6 +29,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
