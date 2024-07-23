499e8c557cac17110906520e9c0670bb61a2a348
mptech <mgoembel1@gmail.com>
2024-07-23 00:25:33 -0400

    Update and rename LetterCombinationOfAPhoneNumber.cpp to medium-17-letter_combination_of_phone_number.cpp

--- Codes/medium-17-letter_combination_of_phone_number.cpp
+++ Codes/medium-17-letter_combination_of_phone_number.cpp
@@ -1,3 +1,27 @@
+// You are given a string containing digits from 2-9 inclusive
+
+// Return all possible letter combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+// Picture Given: 2 -> 'abc' ... 9 -> 'wxyz'
+
+ 
+// Example 1:
+
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+// Example 2:
+
+// Input: digits = ""
+// Output: []
+
+// Example 3:
+
+// Input: digits = "2"
+// Output: ["a","b","c"]
+
+
 class Solution {
     void get_results(vector<string> &res, string nah, string &digits, int i){
         if(i>=digits.size()){
