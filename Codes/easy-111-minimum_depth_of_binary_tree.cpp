daedfb980e739380235cadac1655ac3ee4ada776
Matthew Goembel <mgoembel1@gmail.com>
2024-07-22 21:05:42 -0400

    Update and rename BinaryTree_MinimumDepth.cpp to easy-111-minimum_depth_of_binary_tree.cpp

--- Codes/easy-111-minimum_depth_of_binary_tree.cpp
+++ Codes/easy-111-minimum_depth_of_binary_tree.cpp
@@ -1,3 +1,25 @@
+// You are given a binary tree.
+
+// Find and return its minimum depth.
+
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+// Note: A leaf is a node with no children.
+
+ 
+
+// Example 1:
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 2
+
+// Example 2:
+
+// Input: root = [2,null,3,null,4,null,5,null,6]
+// Output: 5
+
+    
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,6 +31,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
 class Solution {
 public:
     int minDepth(TreeNode* root) {
