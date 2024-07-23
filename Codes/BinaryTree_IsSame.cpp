b683da436c5cab4124cb4874d1f6b9e3d75e49f7
Matthew Goembel <mgoembel1@gmail.com>
2024-07-22 20:52:35 -0400

    Update BinaryTree_IsSame.cpp

--- Codes/BinaryTree_IsSame.cpp
+++ Codes/BinaryTree_IsSame.cpp
@@ -1,3 +1,28 @@
+// You are given the roots of two binary trees p and q, 
+
+// Write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+
+
+// Example 1:
+
+// Input: p = [1,2,3], q = [1,2,3]
+// Output: true
+
+// Example 2:
+
+//Input: p = [1,2], q = [1,null,2]
+// Output: false
+
+// Example 3:
+
+// Input: p = [1,2,1], q = [1,1,2]
+// Output: false
+
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,6 +34,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
