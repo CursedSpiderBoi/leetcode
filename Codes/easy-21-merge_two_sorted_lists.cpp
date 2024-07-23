3dde5063aecaec8e80dcf95893fc9ffbd7229b66
mptech <mgoembel1@gmail.com>
2024-07-23 00:41:08 -0400

    Update and rename MergeTwoSortedLinkedList.cpp to easy-21-merge_two_sorted_lists.cpp

--- Codes/easy-21-merge_two_sorted_lists.cpp
+++ Codes/easy-21-merge_two_sorted_lists.cpp
@@ -1,3 +1,27 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+
+
+// Example 1:
+
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,6 +32,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
